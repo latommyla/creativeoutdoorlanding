323e822b1e7e906d842e0ba23c988c13190b95d0
Tommy La <tommyl.dmd@gmail.com>
2022-09-01 14:40:39 -0700

    added alt text to logo

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,7 +7,7 @@ export default function Homepage() {
         <div class="bg-cover h-screen w-screen">
             <div class="md:mx-auto h-screen flex">
                 <div class="text-center text-white mx-auto font-bebas">
-                    <img src={Logo} class="m-auto md:ml-2 h-44 md:h-56"/>
+                    <img src={Logo} class="m-auto md:ml-2 h-44 md:h-56" alt="Creative Outdoor Logo" title="Creative Outdoor Logo"/>
                     <div class="md:w-screen m-auto md:m-auto">
                         <div class="m-auto mt-0 sm:mt-44 md:mt-44 lg:mt-96">
                         <span class="m-auto text-5xl md:text-7xl drop-shadow-lg">Market leader</span>
